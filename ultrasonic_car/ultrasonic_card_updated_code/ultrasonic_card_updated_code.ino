f6d7f063535cf4d245e8eb02777080152567949a
Sushant <sushant.shah222@gmail.com>
2021-12-28 21:08:57 +0545

    ultrasonic code updated

---
+++ ultrasonic_car/ultrasonic_card_updated_code/ultrasonic_card_updated_code.ino
@@ -0,0 +1,108 @@
+const int LeftMotorForward = 4;
+const int LeftMotorBackward = 5;
+const int RightMotorForward = 6;
+const int RightMotorBackward = 7;
+
+#define trigPin 6
+#define echoPin 13
+
+
+void setup() {
+
+  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+
+  pinMode(RightMotorForward, OUTPUT);
+  pinMode(LeftMotorForward, OUTPUT);
+  pinMode(LeftMotorBackward, OUTPUT);
+  pinMode(RightMotorBackward, OUTPUT);
+}
+
+void Stop()       // Stop Function for Motor Driver.
+{
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(LeftMotorBackward, LOW);
+}
+
+void Forward()    //  Forward Function for Motor Driver.
+{
+    digitalWrite(RightMotorForward, HIGH);
+    digitalWrite(RightMotorBackward, LOW);
+    digitalWrite(LeftMotorForward, HIGH);
+    digitalWrite(LeftMotorBackward, LOW);
+}
+
+void Backward()   //  Backward Function for Motor Driver.
+{
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(RightMotorBackward, HIGH);
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(LeftMotorBackward, HIGH);
+}
+
+void Right()      // Right Function for Motor Driver.
+{
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(RightMotorBackward, HIGH);
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(LeftMotorBackward, LOW);
+}
+
+void Left()       // Left Function for Motor Driver.
+{
+  digitalWrite(RightMotorForward, HIGH);
+  digitalWrite(RightMotorBackward, LOW);
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(LeftMotorBackward, HIGH);
+}
+void loop() {
+  
+  float Time, distance;
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+ 
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  
+  digitalWrite(trigPin, LOW);
+  
+  Time = pulseIn(echoPin, HIGH);
+  distance = (Time / 2) * 0.0344;
+  
+  if (distance >= 400 || distance <= 2){
+    Serial.print("Distance = ");
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print("Distance = ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(500);
+  }
+  delay(500);
+
+
+  
+  if (distance <= 8)        //Algorithm
+  {
+    Stop();
+    delay(300);
+    Backward();
+    delay(400);
+    Stop();
+    delay(300);
+    Right();
+    delay(300);
+  }
+    
+    else
+  {
+    Forward(); 
+  }
+
+
+}
