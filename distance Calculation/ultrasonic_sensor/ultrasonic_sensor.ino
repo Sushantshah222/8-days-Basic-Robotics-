b9e437c4fa74899504880db3e8744b1978691e25
Sushant <sushant.shah222@gmail.com>
2021-12-29 20:21:12 +0545

    IR sensor's slide added

--- distance Calculation/ultrasonic_sensor/ultrasonic_sensor.ino
+++ distance Calculation/ultrasonic_sensor/ultrasonic_sensor.ino
@@ -28,7 +28,7 @@ void loop() {
     Serial.print("Distance = ");
     Serial.print(distance);
     Serial.print(" cm");
-    Serial.print(" ")
+    Serial.println(" ")
     delay(500);
   }
   delay(500);
