592a116918d77e05d763b337967505a220cc9763
Sushant Shah <sushant.shah222@gmail.com>
2025-07-08 13:24:53 +0545

    Fix analog voltmeter reading

--- analog_voltmeter/analog_voltmeter.ino
+++ analog_voltmeter/analog_voltmeter.ino
@@ -1,16 +1,18 @@
+// Pin used to read the analog voltage
+const int readPin = A4;
+
+// variable to store the measured value
+int V2 = 0;
+
 void setup() {
-  // put your setup code here, to run once:
-  int readPin = A4;
-  int V2 = 0;
-  pinMode(readPin,INPUT);
-
+  // configure the analog pin and serial port
+  pinMode(readPin, INPUT);
   Serial.begin(9600);
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  v2 = analogWrite(readPin):
+  // read the voltage level on the analog pin
+  V2 = analogRead(readPin);
   Serial.println(V2);
-
+  delay(500);
 }
